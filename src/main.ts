e7e916ad801993efb641651353b7fa4145e577db
Wado1604 <jezs1604@gmail.com>
2025-10-19 20:23:27 -0700

    Add user-management alias routes and remove debug script

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/AppModule';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
